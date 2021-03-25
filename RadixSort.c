3fd1b4106b8c33e41800628133eb70021fef6045
Aditya Kumar <aryan68125@gmail.com>
2021-03-25 10:30:51 +0530

    Add files via upload

--- RadixSort.c
+++ RadixSort.c
@@ -25,6 +25,8 @@ void createArray()
 	//memory allocation in RAM for all the participating arrays
 	A=(int*)malloc(sizeA*sizeof(int)); //allocating memory to array A dynamically (INPUT ARRAY)
 	B=(int*)malloc(sizeA*sizeof(int)); //allocating memory to array B dynamically (OUTPUT ARRAY)
+
+	// Counting Sort Array - required for arranging digits [0-9]
 	C=(int*)malloc(sizeC*sizeof(int)); //allocating memory to array C dynamically (the counter array)
     
     //now we will enter the elements in the input array one by one
@@ -59,39 +61,81 @@ void Display()
 	printf("\n");
 
     //printing the count array for testing and debugging purposes its temporary
+    printf("printing counter array\n");
     for(int i=0;i<=9;i++)
     {
     	printf("%d ", C[i]);
     }
+    printf("\n");
 }//Display function closed
 
 //...........................................................................................................
 
 //RAdix Sort calculation
-void radixSort()
-{
-	int j=10; //j and k is used to extract the digits from the number
-	int k=1;  //formula used digit = (number%j)/k; where [update j by j= j*10 and k= k*10];
-	int number; //this will isolate the number from the array
-   int digit = 0;
-   for(int m=o;m<LengthA;m++) //this loop will give us numbers from input array
-   {
-
-       number = A[m];
-       for(int i=10000;i>0;i=i/10) //this for loop will decide how many times digit extraction will take place from the number 
-   {
-       digit = (number%j)/k; //formula to calculate digit
-       j=j*10;
-       k=k*10;
-       for(int L=0;L<=digit;L++) //loop for counter array
-       {
-          C[L]=C[L]+1;
-       }
-   }
-
-   }
-
-}//radixSort function is closed
+// Function that performs Radix Sort
+void radix_sort(int arr[], int n){
+ 
+    // Step 1: Find the maxumum element
+    int maximum = arr[0]; //we are sorting this input array in ascending order hence we are taking maximum number here 
+                          // if we wanted to sort this array in descending order then we would have taken minimum = arr[0] here
+ 
+    for(int i=1;i<n;i++) //traversing input array from 0 to LengthA
+    {
+        if(maximum < arr[i])   //here we would have put the condition minimum > arr[i] if we wanted to sort the array in descending order
+            maximum = arr[i];
+    }
+ 
+    // Step 2: Count the number of digits of the maximum number
+    int digits = 0;
+ 
+    while(maximum > 0){
+        digits++;
+        maximum /= 10;
+    }
+ 
+    // Units/Tens/Hundreds - used to determine which digit
+    int power = 1;
+ 
+    // Step 3, 4, 5: Arrange the numbers on the basis of digits
+    for(int i=0;i<digits;i++){
+  
+        // Initializing Count Array
+        for(int j=0;j<10;j++)
+            C[j] = 0;
+ 
+        // Calculating frequency of digits
+        for(int j=0;j<n;j++){
+ 
+            // The digit under consideration in this iteration
+            int num = (arr[j]/power) % 10;
+ 
+            C[num]++;
+        }
+ 
+        // Cumulative frequency of count array
+        for(int j=1;j<10;j++){
+            C[j] += C[j-1];
+        }
+ 
+        // Designating new positions in the updated array
+        for(int j=n-1;j>=0;j--){
+ 
+            // The digit under consideration in this iteration
+            int num = (arr[j]/power) % 10;
+ 
+            B[C[num]-1] = arr[j];
+            C[num]--;
+        }
+ 
+        // Updating the original array using New Array
+        for(int j=0;j<n;j++)
+            arr[j] = B[j];
+         
+        // Updating the digit to be considered next iteration
+        power *= 10;
+    }
+    printf("Radix sort in ascending order is complete\n");           
+}
 
 int main()
 {
@@ -101,7 +145,8 @@ int main()
 	do{
         printf("Press 1 to create Array that holds the elements that you want to sort\n");
         printf("Press 2 to Display the created array\n");
-        printf("Press 3 to Terminate Program\n");
+        printf("Press 3 to radix sort the elements of the array\n");
+        printf("Press 4 to Terminate Program\n");
         scanf("%d",&choice); //entering the choice for selecting the menue items
         switch(choice)
         {
@@ -113,6 +158,9 @@ int main()
                  Display();
          break;
          case 3:
+                radix_sort(A, LengthA);
+         break;
+         case 4:
                  k=7000;
                  printf("Program Terminated by the user\n");
          break;
